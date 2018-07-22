e5ccee89d070a6807dfb07a412362371b446d4cc
Roman TARASENKO <rtarasen@e1r5p3.unit.ua>
2018-07-22 17:14:08 +0300

    deleted unused files

--- my_modules/simple-validator/lib/isEmail.js
+++
@@ -1,7 +0,0 @@
-function isEmail(str) {
-	// Fucking insane regexp for emails that i found on http://emailregex.com/
-	var isValidEmail = new RegExp ('/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/');
-	return isValidEmail.test(str);
-};
-
-module.exports = isEmail;
