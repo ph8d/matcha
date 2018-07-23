1795f61e36e72e9f9770c4b6d31e48e471080416
Roman TARASENKO <rtarasen@e1r5p1.unit.ua>
2018-07-23 18:59:25 +0300

    deleted some comments

--- models/user.js
+++ models/user.js
@@ -1,29 +1,5 @@
 const db = require('../services/db');
 
-// I need to fix this repeating code for error handling
-// And also i need to decide how to pass query results farther, by callback functions or by promises
-
-
-// Promises example
-function imgLoad(url) {
-    return new Promise(function(resolve, reject) {
-        var request = new XMLHttpRequest();
-        request.open('GET', url);
-        request.responseType = 'blob';
-        request.onload = function() {
-            if (request.status === 200) {
-                resolve(request.response);
-            } else {
-                reject(Error('Image didn\'t load successfully; error code:' + request.statusText));
-            }
-        };
-        request.onerror = function() {
-            reject(Error('There was a network error.'));
-        };
-        request.send();
-    });
-}
-
 exports.logAllUsers = () => {
     db.get()
         .then(connection => {
