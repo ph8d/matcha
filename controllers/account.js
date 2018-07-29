d58d613afe42ecc6f4734156fdfb6bc6951fbde2
Roman TARASENKO <rtarasen@e1r6p5.unit.ua>
2018-07-29 17:17:10 +0300

    Registration validation works now but it looks scary

--- controllers/account.js
+++ controllers/account.js
@@ -1,10 +1,9 @@
 const User = require('../models/user');
 const passport = require('passport');
-const validator = require('../lib/registrationValidation');
+const validator = require('../lib/validator');
 const bcrypt = require('bcrypt');
 
 exports.register = (req, res) => {
-	User.findOne({id:5}).then(console.log);
 	let form = {
 		login: '',
 		email: '',
@@ -20,36 +19,39 @@ exports.register = (req, res) => {
 		form.password = req.body.password;
 		form.password_confirm = req.body.password_confirm;
 
-		let validationErrors = registrationValidation(form);
+		/* Validation works but it looks scary, probably requires some refactoring */
 
-		if (validationErrors.length > 0) {
-			validationErrors.forEach(msg => {
-				req.flash('danger', msg);
+		validator.registrationValidation(form)
+			.then(validationErrors => {
+				if (validationErrors.length > 0) {
+					validationErrors.forEach(msg => {
+						req.flash('danger', msg);
+					});
+					res.render('register', {form:form});		
+				} else {
+					delete form.password_confirm;
+					bcrypt.hash(form.password, 12)
+						.then(hash => {
+							form.password = hash;
+							return User.add(form);
+						})
+						.then(insertedId => {
+							console.log('ID of inserted user: ' + insertedId)
+							req.flash('success', 'Registration successful! Please, check your email.');
+							res.redirect('/');
+						})
+						.catch(reason => {
+							console.error(error);
+							req.flash('danger', 'Some server side error occured. Please try again.');
+							res.render('register', {form:form});
+						});
+				}
+			})
+			.catch(error => {
+				console.error(error);
+				req.flash('danger', 'Some server side error occured. Please try again.');
+				res.render('register', {form:form});
 			});
-			res.render('register', {form:form});		
-		} else {
-
-			delete form.password_confirm;
-
-			/* I need to check if user with this email or login already exist! */			
-			/* Maybe i can add isUniqueCredentials function to the top of this promise chain */
-
-			bcrypt.hash(form.password, 12)
-				.then(hash => {
-					form.password = hash;
-					return User.add(form);
-				})
-				.then(insertedId => {
-					console.log('ID of inserted user: ' + insertedId)
-					req.flash('success', 'Registration successful! Please, check your email.');
-					res.redirect('/');
-				})
-				.catch(error => {
-					console.error(error);
-					req.flash('danger', 'Some server side error occured, please try again.');
-					res.render('register', {form:form});
-				});
-		}
 	} else {
 		res.render('register', {form:form});
 	}
